71d66b85d8a18d3481e012c06c19c04dae5b8573
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2019-06-18 10:16:07 +0200

    refactor

--- aiakos/src/components/Aiakos.vue
+++ aiakos/src/components/Aiakos.vue
@@ -18,63 +18,56 @@
       </b-form-group>
     </b-modal>
     <b-card class="mt-3" header="Owner" v-if="showOwnerPanel">
+
       <b-form @submit="addMaintainer" @reset="onOwnerPanelReset">
-        <!--b-form-group
-          id="input-group-maintainer-address"
-          label="Maintainer address:"
-          label-for="input-maintainer-address"
-          description="Address of the maintainer."
-        >
-          <b-form-input
-            id="input-maintainer-address"
-            v-model="maintainerAddress"
-            required
-            placeholder="Enter maintainer address (prefixed by 0x)"
-          ></b-form-input>
-        </b-form-group-->
-        <b-form-group id="input-group-maintainer-address" label="Maintainer:" label-for="input-maintainer-address">
-          <b-form-select
-            id="input-maintainer-address"
-            v-model="maintainerAddress"
-            :options="accounts"
-            required
-          ></b-form-select>
-        </b-form-group>
-        <b-button type="submit" variant="primary">Add maintainer</b-button>
-        <b-button v-on:click="isMaintainer" variant="success">Is maintainer</b-button>
-        <b-button type="reset" variant="danger">Reset</b-button>
+        <b-button-toolbar aria-label="Owner toolbar">
+          <b-input-group size="m" prepend="maintainer">
+            <b-form-select
+              id="input-maintainer-address"
+              v-model="maintainerAddress"
+              :options="accounts"
+              required
+            ></b-form-select>
+            <b-button-group size="sm" class="mr-1">
+              <b-button type="submit" variant="primary">Add maintainer</b-button>
+            </b-button-group>
+            <b-button-group size="sm" class="mr-1">
+              <b-button v-on:click="isMaintainer" variant="success">Is maintainer</b-button>
+            </b-button-group>
+            <b-button-group size="sm" class="mr-1">
+              <b-button type="reset" variant="danger">Reset</b-button>
+            </b-button-group>
+          </b-input-group>
+        </b-button-toolbar>
       </b-form>
     </b-card>
     <b-card class="mt-3" header="Maintainer" v-if="showMaintainerPanel">
       <b-form @submit="release">
-        <b-form-group
-          id="input-group-release-version"
-          label="Release version:"
-          label-for="input-release-version"
-          description="Version of the release."
-        >
-          <b-form-input
-            id="input-release-version"
-            v-model="releaseVersion"
-            required
-            placeholder="Version string. (1.0.0)"
-          ></b-form-input>
-        </b-form-group>
-        <b-form-group
-          id="input-group-release-hash"
-          label="Release hash:"
-          label-for="input-release-hash"
-          description="Hash of the release."
-        >
-          <b-form-input
-            id="input-release-hash"
-            v-model="releaseHash"
-            required
-            placeholder="SHA-256 Hash"
-          ></b-form-input>
-        </b-form-group>
-        <b-button v-on:click="randomHash" type="Random">Random</b-button>
-        <b-button type="submit" variant="primary">Release</b-button>
+        <b-button-toolbar aria-label="Release toolbar">
+          <b-input-group size="m" prepend="version">
+            <b-form-input
+              id="input-release-version"
+              v-model="releaseVersion"
+              required
+              placeholder="Version string. (1.0.0)"
+            ></b-form-input>
+          </b-input-group>
+          <b-input-group size="m" prepend="hash">
+            <b-form-input
+              id="input-release-hash"
+              v-model="releaseHash"
+              class="text-right"
+              required
+              placeholder="SHA-256 Hash"
+            ></b-form-input>
+            <b-button-group size="sm" class="mr-1">
+              <b-button v-on:click="randomHash">Random</b-button>
+            </b-button-group>
+            <b-button-group size="sm" class="mr-1">
+              <b-button type="submit" variant="primary">Release</b-button>
+            </b-button-group>
+          </b-input-group>
+        </b-button-toolbar>
       </b-form>
     </b-card>
   </div>
@@ -84,22 +77,17 @@
   import contractArtifacts from '../assets/contract.json';
   import getWeb3 from '../utils/getWeb3';
 
-  let web3;
-  let account;
-  getWeb3.then(results => {
-    web3 = results.web3;
-    account = results.account;
-  });
-
   export default {
     name: 'Aiakos',
-    /*beforeCreate() {
-      console.log('Initializing Aiakos vue.');
-    },*/
+    created() {
+      console.log('Initializing Aiakos vue blockchain configuration.');
+      getWeb3(this.contractAddress, contractArtifacts).then(value => {
+        this.web3Config = value;
+      });
+    },
     data() {
       return {
-        web3: null,
-        account: null,
+        web3Config: null,
         title: 'Aiakos: Release on the blockchain.',
         contractAddress: '0x9c253D6F4A0b9269AEce386936372236BAE10007',
         maintainerAddress: '',
@@ -119,11 +107,8 @@
     },
     methods: {
       addMaintainer(evt) {
-        console.log("addMaintainer: " + this.maintainerAddress);
-        let contract = new web3.eth.Contract(contractArtifacts.abi);
-        contract.options.address = this.contractAddress;
-        contract.setProvider(web3.currentProvider);
-        contract.methods.addMaintainer(this.maintainerAddress).send({from: account}, function (error, result) {
+        console.log(">> addMaintainer: " + this.maintainerAddress);
+        this.web3Config.contract.methods.addMaintainer(this.maintainerAddress).send({from: this.web3Config.account}, function (error, result) {
           if (error) {
             console.log("addMaintainer error.");
             console.log(error);
@@ -134,13 +119,18 @@
       },
       isMaintainer(evt) {
         console.log("isMaintainer: " + this.maintainerAddress);
-        let contract = new web3.eth.Contract(contractArtifacts.abi);
-        contract.options.address = this.contractAddress;
-        contract.setProvider(web3.currentProvider);
-        contract.methods.isMaintainer(this.maintainerAddress).call();
+        this.web3Config.contract.methods.isMaintainer(this.maintainerAddress).call();
       },
       release(evt) {
         console.log("release version: " + this.releaseVersion);
+        this.web3Config.contract.methods.deployRelease(this.releaseVersion, this.releaseHash).send({from: this.web3Config.account}, function (error, result) {
+          if (error) {
+            console.log("release error.");
+            console.log(error);
+          } else {
+            console.log("release success.");
+          }
+        });
       },
       randomHash(evt) {
         this.releaseHash = web3.utils.randomHex(32);
--- aiakos/src/utils/getWeb3.js
+++ aiakos/src/utils/getWeb3.js
@@ -1,6 +1,6 @@
 import Web3 from 'web3'
 
-let getWeb3 = new Promise(function (resolve, reject) {
+/*let getWeb3 = new Promise(function (resolve, reject) {
   window.addEventListener('load', async () => {
     let results;
     let account;
@@ -24,6 +24,45 @@ let getWeb3 = new Promise(function (resolve, reject) {
       console.log('User denied account access.');
     }
   });
-});
+});*/
+
+async function getWeb3 (contractAddress, contractArtifacts) {
+  let promise = new Promise((resolve, reject) => {
+    window.addEventListener('load', async () => {
+      let results;
+      let account = null;
+      let web3;
+      let contract = null;
+      window.web3 = new Web3(ethereum);
+      try {
+        // Request account access if needed
+        await ethereum.enable();
+        // Acccounts now exposed
+        web3 = window.web3;
+        await web3.eth.getAccounts().then(accounts => {
+          account = accounts[0];
+        });
+        contract = new web3.eth.Contract(contractArtifacts.abi);
+        contract.options.address = contractAddress;
+        await contract.setProvider(web3.currentProvider);
+        results = {
+          web3: web3,
+          account: account,
+          contract: contract,
+        };
+        resolve(results);
+      } catch (error) {
+        // User denied account access...
+        reject(Error('User denied account access.'));
+      }
+    });
+  });
+  let result = await promise;
+  return {
+    web3: result.web3,
+    account: result.account,
+    contract: result.contract,
+  };
+}
 
 export default getWeb3
