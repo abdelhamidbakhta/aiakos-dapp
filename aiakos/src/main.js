6e252131cba846f32fd86cb69ad143b915514be6
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2019-06-21 18:56:45 +0200

    ui improvements

--- aiakos/src/main.js
+++ aiakos/src/main.js
@@ -8,17 +8,15 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon'
-
 Vue.component('v-icon', Icon);
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 });
